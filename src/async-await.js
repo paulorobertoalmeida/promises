9da8a55e3ce0a6a1f24ce4ea72f55702a67b745d
Paulo Almeida <paulo.roberto.a@hotmail.com>
2022-07-29 07:47:39 +0200

    feat: Async Await Space X Patch Images

--- src/async-await.js
+++ src/async-await.js
@@ -140,3 +140,29 @@ const URL = "https://api.spacexdata.com/v4/launches";
   
 //   displayMissionPatches();
   
+
+//Displaying the patch Images
+async function displayMissionPatches(limit = 0) {
+    try {
+      const response = await fetch("https://api.spacexdata.com/v4/launches");
+      const jsonResponse = await response.json();
+  
+      console.log(jsonResponse);
+      const launchesToDisplay = jsonResponse.slice(0, limit);
+  
+      launchesToDisplay.forEach((launchObj) => {
+        const patchImage = launchObj.links.patch.small;
+        const imgElement = document.createElement("img");
+  
+        imgElement.setAttribute("src", patchImage);
+        imgElement.setAttribute("width", 200);
+        document.body.appendChild(imgElement);
+      });
+    } catch (error) {
+      // Handle error or a rejected Promise
+      console.log("Something went wrong!", error);
+    }
+  }
+  
+  displayMissionPatches(10);
+  
